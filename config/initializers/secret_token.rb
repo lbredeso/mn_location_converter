6081f6842bcbd05d3c62efa6d9c8644c32fa6c83
Luke Bredeson <luke.bredeson@gmail.com>
2012-02-02 21:19:57 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MnLocationConverter::Application.config.secret_token = '1d37175a320f4d0fd98649f56333f21d4dc6528ee6f61ccbb79c6bf870dc8bc5c9f60e1d31ed07a603778a99a6c39cf108d4e0a65a17b10cd70bea4eaef3dab4'
