2f8a6174914175f5dffccd092805b1b7d4eea8a4
Luke Bredeson <luke.bredeson@gmail.com>
2012-02-08 23:02:51 -0600

    Fix for bad point math; probably picked the right side of begm/endm to exclude; don't use roads in decreasing direction

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,18 +1,22 @@
 class Event < ActiveRecord::Base
-  def self.find_lat_lon
+  def self.find_lat_lon max
     self.find_by_sql <<-SQL
       select st_x(point) as longitude, st_y(point) as latitude, id, unique_id, road_id, distance from 
         (
           select st_line_interpolate_point(
             st_transform(LineMerge(roads.the_geom), 4326), 
-            events.distance / st_length(roads.the_geom)
+            (events.distance - roads.begm) / (roads.endm - roads.begm)
           ) as point, events.id, events.unique_id, events.road_id, events.distance
           from events
           join roads on 
-            events.road_id = roads.tis_code AND 
-            events.distance > roads.begm AND 
-            events.distance <= roads.endm
-        ) as event limit 10;
+            events.road_id = roads.tis_code and 
+            events.distance >= roads.begm and 
+            events.distance < roads.endm
+          where
+            roads.traf_dir in ('B', 'I')
+          order by
+            events.id
+        ) as event limit #{max}
     SQL
   end
 end
